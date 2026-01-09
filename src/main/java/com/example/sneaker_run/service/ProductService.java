88d68d4e82ce16ebee31d75e807f231a045e0951
mcy0515 <mcy0515@naver.com>
2026-01-09 15:57:23 +0900

    feat: 상품 목록 조회 API 구현

--- src/main/java/com/example/sneaker_run/service/ProductService.java
+++ src/main/java/com/example/sneaker_run/service/ProductService.java
@@ -6,6 +6,8 @@ import com.example.sneaker_run.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import com.example.sneaker_run.dto.ProductResponse;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -27,5 +29,17 @@ public class ProductService {
         Product savedProduct = productRepository.save(product);
 
         return savedProduct.getId();
+    }
+
+    @Transactional(readOnly = true) // 조회 전용이라 readOnly 걸면 성능 좋아짐
+    public List<ProductResponse> getAllProducts() {
+        return productRepository.findAll().stream() // 1. 모든 상품 가져오기
+                .map(product -> new ProductResponse(    // 2. 하나씩 DTO로 변환
+                        product.getId(),
+                        product.getName(),
+                        product.getPrice(),
+                        product.getStockQuantity()
+                ))
+                .toList(); // 3. 리스트로 묶어서 반환
     }
 }
