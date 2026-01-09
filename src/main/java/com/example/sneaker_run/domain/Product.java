1f211a1572137e3f25ca16939d30dd670d550442
mcy0515 <mcy0515@naver.com>
2026-01-09 16:37:20 +0900

    feat: 주문 API 구현 및 재고 차감 로직 추가

--- src/main/java/com/example/sneaker_run/domain/Product.java
+++ src/main/java/com/example/sneaker_run/domain/Product.java
@@ -25,4 +25,13 @@ public class Product {
         this.stockQuantity = stockQuantity;
         this.description = description;
     }
+
+    // 재고 감소 로직 (비즈니스 로직)
+    public void removeStock(Long quantity) {
+        long restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new IllegalArgumentException("재고가 부족합니다."); // 예외 발생!
+        }
+        this.stockQuantity = restStock;
+    }
 }
